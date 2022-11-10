aefaf2cab1b5b8695e7279739f9dd0024894f1c9
kimchaeeun3447 <rlacodms3447@naver.com>
2022-11-11 00:40:05 +0900

    add : android initial setting
    안드로이드 스튜디오 초기 세팅

---
+++ app/src/main/java/com/example/likelion10th_sideproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.likelion10th_sideproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
